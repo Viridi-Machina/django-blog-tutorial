0997c7725f4fddd33c5338a6f84a5d2681901511
jacen green <jacen.l.green@hotmail.com>
2024-05-29 14:47:47 +0100

    fix missing allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(x6+sx*n1o!n^-&#@-9#ch$wd_xh4@&13muuhs36hjqjit2!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
